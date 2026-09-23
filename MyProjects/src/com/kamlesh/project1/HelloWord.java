e0319ff946924a48524cb6eb353eecf5c716c8bc
kamleshpant <kamleshpantk14@gmail.com>
2026-09-23 18:47:30 +0530

    added new changes in helloWord file

--- MyProjects/src/com/kamlesh/project1/HelloWord.java
+++ MyProjects/src/com/kamlesh/project1/HelloWord.java
@@ -3,6 +3,7 @@ package com.kamlesh.project1;
 public class HelloWord {
 	public static void main(String args[]) {
 		System.out.println("Hello Word");
+		System.out.println("JAVA Git Code");
 	}
 
 }
