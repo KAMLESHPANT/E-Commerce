402e79732768c5cdc36ccecb6c57d72ee2de0699
kamleshpant <kamleshpantk14@gmail.com>
2026-09-23 19:00:52 +0530

    Added For Loop

--- MyProjects/src/com/kamlesh/project1/HelloWord.java
+++ MyProjects/src/com/kamlesh/project1/HelloWord.java
@@ -4,6 +4,11 @@ public class HelloWord {
 	public static void main(String args[]) {
 		System.out.println("Hello Word");
 		System.out.println("JAVA Git Code");
+		
+		for (int i=0;i<10;i++)
+		{
+			System.out.println(i);
+		}
 	}
 
 }
